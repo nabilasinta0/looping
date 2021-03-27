47c78bcc4166e88d006e589647217166e65e53a2
Nabila Sinta <68362834+nabilasinta0@users.noreply.github.com>
2021-03-27 12:23:59 +0700

    Add files via upload

---
+++ loops.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+<?php 
+    // Looping While
+    echo '<h3>No. 1 Looping While </h3>';
+    echo "LOOPING PERTAMA<br />";
+	$i = 2;
+	while ($i <= 20) {
+		echo $i. ' - I Love coding <br/>' ;
+		$i+=2;
+	}
+    echo "<br/>";
+
+    echo "LOOPING KEDUA<br />";
+    $a = 20;
+	while ($a >= 2) {
+		echo $a. ' - I will become a mobile developer <br/>' ;
+		$a-=2;
+	}
+    
+	// Looping for
+	echo '<h3>No. 2 Looping menggunakan for </h3>';
+?>
+</body>
+</html>
